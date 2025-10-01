108ecbed17ea75a3a554faa387f077fd6776498a
rushikeshkinhale657-dotcom <rushikeshkinhale657@gmail.com>
2025-10-02 00:46:59 +0530

    Add files via upload

---
+++ 18.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100], rev[100];
+    int i, j, len;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // remove newline if fgets stores it
+    str[strcspn(str, "\n")] = '\0';
+
+    len = strlen(str);
+
+    for (i = 0, j = len - 1; i < len; i++, j--) {
+        rev[i] = str[j];
+    }
+    rev[len] = '\0';
+
+    printf("Reversed string = %s\n", rev);
+
+    return 0;
+}
