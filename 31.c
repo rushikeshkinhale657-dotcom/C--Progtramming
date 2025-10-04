cdd09309de0f32df9d53d10dff2785cfb760eda5
rushikeshkinhale657-dotcom <rushikeshkinhale657@gmail.com>
2025-10-04 07:59:04 +0530

    Add files via upload

---
+++ 31.c
@@ -0,0 +1,9 @@
+
+
+#include <stdio.h>
+int gcd(int a,int b){ while(b){int t=a%b;a=b;b=t;} return a; }
+int main() {
+    int a,b; scanf("%d %d",&a,&b);
+    printf("%d\n", (a*b)/gcd(a,b));
+    return 0;
+}
